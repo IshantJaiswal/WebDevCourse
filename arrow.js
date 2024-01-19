e4664546c4e079118484105c9fa063f5f32a7dc5
IshantJaiswal <117654993+IshantJaiswal@users.noreply.github.com>
2024-01-19 17:42:09 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,7 @@
+function square(number) {
+    return number * number;
+  }
+  const givenNumber = 2;
+  const result = square(givenNumber);
+  
+  console.log("The square of " + givenNumber + " is: " + result);
